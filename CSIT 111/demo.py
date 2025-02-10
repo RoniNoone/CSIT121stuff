65657fa561e25055d9efa64cd7b0530581c02f7e
Roni Noone <38510282+RoniNoone@users.noreply.github.com>
2025-02-10 13:37:40 -0500

    Add files via upload

---
+++ CSIT 111/demo.py
@@ -0,0 +1,13 @@
+#veronica noone
+#demo program for retirement age
+
+#declare constant
+AGE_TO_RETIRE = 65
+
+print('Welcome to me retirement age program!')
+print('How old are you?')
+age = input()
+retAge = AGE_TO_RETIRE - int(age)
+print('Hey! You have', retAge, 'years to retirement.')
+
+
